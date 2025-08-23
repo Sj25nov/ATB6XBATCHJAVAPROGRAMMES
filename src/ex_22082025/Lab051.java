2ebecc14034973f4493b315212450d6269d9b934
Sj25nov <saurabhj1414@gmail.com>
2025-08-23 22:35:04 +0530

    22 AUG 2025

---
+++ src/ex_22082025/Lab051.java
@@ -0,0 +1,4 @@
+package ex_22082025;
+
+public class Lab051 {
+}
---
+++ src/ex_22082025/Lab060.java
@@ -0,0 +1,17 @@
+package ex_22082025;
+
+public class Lab60 {
+    public static void main(String[] args) {
+        String s1="Saurabh Jadhav"; //SCP
+        String s2="Saurabh Jadhav";//SCP
+        String s3=new String("Saurabh Jadhav"); //Heap Area
+
+
+        System.out.println(s1==s2); //Check the references from SCP
+        System.out.println(s1.equals(s2)); // Check the vlaues are equal or not
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s3));
+
+}
+
+}
